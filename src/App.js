ec957b51f62c92614af908b16a3442e632e9d12e
Kakasi-Daniel <kakasi.daniel@gmail.com>
2021-08-25 01:09:41 +0300

    added custom properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 import html2canvas from 'html2canvas';
-
 
 const colors = [
   'red',
@@ -20,16 +19,57 @@ const colors = [
 function App() {
   const [gridLayout, setGridLayout] = useState(8);
   const [grid, setGrid] = useState([]);
+  const [colorInput, setColorInput] = useState('');
+  const [gridInput, setGridInput] = useState('');
+  const [colorInputError, setColorInputError] = useState(null);
+  const [gridInputError, setGridInputError] = useState(null);
   const [colorsButtons, setColorsButtons] = useState([]);
+  const [color,_setColor] = useState('red')
+  const colorRef = useRef(color);
   const gridRef = useRef();
-  const colorStateRef = useRef('red');
 
-  const setColor = (color) => {
-    colorStateRef.current = color;
+  const setColor = (col) =>{
+    colorRef.current = col;
+    _setColor(col)
+  }
+
+
+  const colorInputChangeHandler = (e) => {
+    var s = new Option().style;
+    s.color = e.target.value;
+    if(s.color == e.target.value){
+      setColorInputError(null)
+    }else{
+      setColorInputError('errorBorder')
+    }
+    setColorInput(e.target.value)
+  };
+
+  const gridInputChangeHandler = (e) => {
+    if(+e.target.value < 3){
+      setGridInputError('errorBorder')
+    }else{
+      setGridInputError(null)
+    }
+    setGridInput(Math.floor(+e.target.value))
+  };
+
+  const customColorHandler = (e) => {
+    e.preventDefault()
+    if(!colorInputError && colorInput !== ""){
+      setColor(colorInput)
+    }
+  };
+
+  const customGridHandler = (e) => {
+    e.preventDefault()
+    if(!gridInputError && gridInput !== ""){
+      setGridLayout(gridInput)
+    }
   };
 
   const downloadArtHandler = () => {
-    html2canvas(gridRef.current).then(canvas => {
+    html2canvas(gridRef.current).then((canvas) => {
       let link = document.createElement('a');
       link.href = canvas.toDataURL();
       link.download = 'yourPixArt';
@@ -40,18 +80,18 @@ function App() {
   };
 
   const changeCellColorHandler = (e) => {
-    if (e.target.style.background === colorStateRef.current) {
+    if (e.target.style.background === colorRef.current) {
       e.target.style.background = 'rgb(255,255,255)';
     } else {
-      e.target.style.background = colorStateRef.current;
+      e.target.style.background = colorRef.current;
     }
   };
 
-  const changeMainColorHandler = (e) => {
+  const changeMainColorHandler = useCallback((e) => {
     let colorToChange = '';
     colorToChange = e.target.style.backgroundColor;
     setColor(colorToChange);
-  };
+  }, []);
 
   useEffect(() => {
     const colorsBtns = [];
@@ -67,7 +107,7 @@ function App() {
     });
 
     setColorsButtons(colorsBtns);
-  }, []);
+  }, [changeMainColorHandler]);
 
   useEffect(() => {
     let rows = [];
@@ -109,6 +149,42 @@ function App() {
           </button>
         </div>
         <div className="controls">{colorsButtons}</div>
+        <div className="controls">
+          <form onSubmit={customColorHandler} className="custom custom--color">
+            <label htmlFor="color">
+              Custom color:
+              <input
+              className={colorInputError}
+                value={colorInput}
+                onChange={colorInputChangeHandler}
+                type="text"
+                id="color"
+                placeholder="pink"
+              />
+            </label>
+            <button type="submit" className="btn btn--set">
+              Set
+            </button>
+          </form>
+          <div className="currentColor" style={{backgroundColor:color}}></div>
+          <form onSubmit={customGridHandler} className="custom custom--grid">
+            <label htmlFor="grid">
+              Custom grid:
+              <input
+              className={gridInputError}
+                value={gridInput}
+                onChange={gridInputChangeHandler}
+                placeholder="3 to infinity"
+                type="number"
+                min="3"
+                id="grid"
+              />
+            </label>
+            <button type="submit" className="btn btn--set">
+              Set
+            </button>
+          </form>
+        </div>
         <div ref={gridRef} className="pixelsgrid">
           {grid.map((row, index) => (
             <div key={`row${index + 1}`} className="row">
